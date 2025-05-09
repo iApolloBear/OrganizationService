541434c0737cff29e2cdc31d7c1cb191b6d7f7f9
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-05-09 15:10:35 -0600

    Organization repository created.

---
+++ src/main/java/com/optimagrowth/organization_service/repository/OrganizationRepository.java
@@ -0,0 +1,6 @@
+package com.optimagrowth.organization_service.repository;
+
+import com.optimagrowth.organization_service.model.Organization;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrganizationRepository extends CrudRepository<Organization, String> {}
